cd5d3b53ace0287f2a37553c52a6d0349a082820
Necto <necto.ne@gmail.com>
2015-12-06 11:38:26 +0100

    first step towards Klee integration

---
+++ softboundcets-lib/mmap-stub.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "mymmap.h"
+
+char bigarray[PREALLOCATED_SIZE];
+size_t allocated = 0;
+
+void* mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset) {
+    assert(addr == 0);
+    assert(prot == (PROT_READ | PROT_WRITE));
+    assert(flags == (MAP_PRIVATE|MAP_ANONYMOUS|MAP_NORESERVE));
+    assert(offset == 0);
+    assert(fd == -1);
+    
+    assert(allocated + length < PREALLOCATED_SIZE);
+    size_t start = allocated; 
+    allocated += length;
+    printf("allocated %lu\n", length);
+    return &bigarray[start];
+}
---
+++ softboundcets-lib/mmap-stub.h
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <sys/types.h>
+#include <assert.h>
+
+#define PREALLOCATED_SIZE  128*1024*1024UL
+
+#define MAP_PRIVATE 0x02
+#define MAP_ANONYMOUS 0x20
+#define MAP_NORESERVE 0x04000
+
+#define PROT_READ 0x1
+#define PROT_WRITE 0x2
+
+void* mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset);
